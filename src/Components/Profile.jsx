12e399211ec762fcc633a4a1c07681fb7e1fdc11
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2022-04-08 20:48:45 +0200

    add first proj

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>
+                    newpost
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
